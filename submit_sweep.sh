99e6dc9ccc6a0cb820ecbc1d1bc0f0dfd81ed323
lhz1029 <lilyhzhang1029@gmail.com>
2021-03-09 13:21:11 -0500

    add gpu constraint to sbatch script

--- submit_sweep.sh
+++ submit_sweep.sh
@@ -3,7 +3,8 @@
 #SBATCH --mem-per-gpu=6G
 #SBATCH --array=1-20
 #SBATCH --gres=gpu:1
-#SBATCH --exclude=hpc1,hpc2,hpc3,hpc4,hpc5,hpc6,hpc7,hpc8,hpc9,vine3,vine4,vine6,vine11,vine12,rose7,rose8,rose9,lion17,lion3,rose1
+#SBATCH --exclude=hpc1,hpc2,hpc3,hpc4,hpc5,hpc6,hpc7,hpc8,hpc9,vine3,vine4,vine6,vine11,vine12,rose7,rose8,rose9,lion17,lion3,rose1,rose[2-4]
+#SBATCH --constraint="pascal|maxwell"
 #SBATCH --time=1-10:00:00
 #SBATCH --constraint="pascal|turing|volta|maxwell"
 
